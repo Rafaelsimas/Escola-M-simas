358025e2727f4ca4c0730e5037fea9178b4f1d37
rafael simas <rafael.simas@al.resilia.com.br>
2021-02-03 20:08:38 -0300

    ajuste nos textos

--- logica.js
+++ logica.js
@@ -1,8 +1,8 @@
 let btn = document.getElementById('btn');
 btn.addEventListener('click', () => {
   let media = 10;
-  let aprovado = 'APROVADO';
-  let reprovado = 'REPROVADO';
+  let aprovado = 'APROVADO(a)';
+  let reprovado = 'REPROVADO(a)';
   let recuperacao = 'RECUPERAÇÃO';
   let nome = document.getElementById('nome');
   let nota = document.getElementById('nota');
@@ -10,19 +10,19 @@ btn.addEventListener('click', () => {
 
   document.body.style.fontFamily = 'arial';
   // aprovado
-  if (nota.value >= media || nota.value == 9) {
-    div.innerHTML = ` <strong style="color:lightgrey;">O aluno</strong> <strong style="color:grey;">${nome.value.toUpperCase()}</strong> <strong style="color:lightgrey;">tirou nota</strong> <strong style="color:lightgrey;">${
+  if (nota.value >= media || nota.value >= 7) {
+    div.innerHTML = ` <strong style="color:lightgrey;">Aluno(a)</strong> <strong style="color:grey;">${nome.value.toUpperCase()}</strong> <strong style="color:lightgrey;">tirou nota</strong> <strong style="color:lightgrey;">${
       nota.value
     } e foi: </strong><strong style="color:green;">${aprovado.toUpperCase()}</strong>`;
 
     // reprovado
-  } else if (nota.value <= 5) {
-    div.innerHTML = `<strong style="color:lightgrey;">O aluno</strong> <strong style="color:grey;">${nome.value.toUpperCase()}</strong> <strong style="color:lightgrey;">tirou nota</strong> <strong style="color:lightgrey;">${
+  } else if (nota.value < 5) {
+    div.innerHTML = `<strong style="color:lightgrey;">Aluno(a)</strong> <strong style="color:grey;">${nome.value.toUpperCase()}</strong> <strong style="color:lightgrey;">tirou nota</strong> <strong style="color:lightgrey;">${
       nota.value
     } e foi: </strong><strong style="color:red;">${reprovado.toUpperCase()}</strong>`;
     // recuperacao
-  } else if (nota.value <= 8 || nota >= 6) {
-    div.innerHTML = `<strong style="color:lightgrey;">O aluno</strong> <strong style="color:grey;">${nome.value.toUpperCase()}</strong> <strong style="color:lightgrey;">tirou nota</strong> <strong style="color:lightgrey;">${
+  } else if (nota.value <= 6 || nota >= 5) {
+    div.innerHTML = `<strong style="color:lightgrey;">Aluno(a)</strong> <strong style="color:grey;">${nome.value.toUpperCase()}</strong> <strong style="color:lightgrey;">tirou nota</strong> <strong style="color:lightgrey;">${
       nota.value
     } e ficou em: </strong><strong style="color:orange;">${recuperacao.toUpperCase()}</strong>`;
   }
